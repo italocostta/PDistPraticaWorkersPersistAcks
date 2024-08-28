2ceeb651d1b7bcd39fcfa9ec003d3ae091f1f27a
italocostta <italo.costta26@gmail.com>
2024-08-28 00:07:39 -0300

    Prática - Workers, Persistência e Acks - RabbitMQ

--- src/main/java/org/example/NewTask.java
+++ src/main/java/org/example/NewTask.java
@@ -10,22 +10,19 @@ public class NewTask {
     private static final String TASK_QUEUE_NAME = "task_queue";
 
     public static void main(String[] argv) throws Exception {
-        // Configuração da conexão com o RabbitMQ
+
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
-
 
         try (Connection connection = factory.newConnection();
              Channel channel = connection.createChannel()) {
 
-            // Declaração da fila com durabilidade ativada
             boolean durable = true;
             channel.queueDeclare(TASK_QUEUE_NAME, durable, false, false, null);
 
-
+            // Inserindo meu nome
             String message = "Olá, sou o aluno Ítalo Costa Soares de Oliveira.";
 
-            // Publicação da mensagem com persistência
             channel.basicPublish("", TASK_QUEUE_NAME,
                     MessageProperties.PERSISTENT_TEXT_PLAIN,
                     message.getBytes("UTF-8"));
--- src/main/java/org/example/Worker.java
+++ src/main/java/org/example/Worker.java
@@ -7,24 +7,18 @@ public class Worker {
     private static final String TASK_QUEUE_NAME = "task_queue";
 
     public static void main(String[] argv) throws Exception {
-        // Configuração da conexão com o RabbitMQ
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
-
-
         final Connection connection = factory.newConnection();
         final Channel channel = connection.createChannel();
 
-        // Declaração da fila com durabilidade ativada
         boolean durable = true;
         channel.queueDeclare(TASK_QUEUE_NAME, durable, false, false, null);
         System.out.println(" [*] Aguardando mensagens. Para sair pressione CTRL+C");
 
-        // Configuração de fair dispatch (envio justo)
         int prefetchCount = 1;
         channel.basicQos(prefetchCount);
 
-        // Callback para processamento das mensagens
         DeliverCallback deliverCallback = (consumerTag, delivery) -> {
             String message = new String(delivery.getBody(), "UTF-8");
             System.out.println(" [x] Recebido: '" + message + "'");
@@ -36,21 +30,17 @@ public class Worker {
                 }
                 System.out.println(" [x] Feito");
             } finally {
-                // Confirmação manual da mensagem
                 channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
             }
         };
-
-        // Iniciando o consumo da fila com acks manuais
-        boolean autoAck = false; // Desabilitando auto-acknowledgement
+        boolean autoAck = false;
         channel.basicConsume(TASK_QUEUE_NAME, autoAck, deliverCallback, consumerTag -> { });
     }
 
-    // Método que simula o processamento da mensagem
     private static void doWork(String task) throws InterruptedException {
         for (char ch : task.toCharArray()) {
             if (ch == '.') {
-                Thread.sleep(1000); // Pausa de 1 segundo para cada ponto
+                Thread.sleep(1000);
             }
         }
     }
